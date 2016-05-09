79437b9fb42f5e7c7aca1f0887208349ebb7722c
dinusha_dck <dinusha@duosoftware.com>
2016-05-09 11:14:50 +0530

    add env variables

---
+++ config/default.js
@@ -0,0 +1,21 @@
+module.exports = {
+  "DB": {
+    "Type":"postgres",
+    "User":"duo",
+    "Password":"DuoS123",
+    "Port":5432,
+    "Host":"104.131.105.222",
+    "Database":"duo"
+  },
+  "Security":
+  {
+    "ip" : "45.55.142.207",
+    "port": 6389,
+    "password": "DuoS123"
+  },
+  "Host":{
+    "Ip":"0.0.0.0",
+    "Port":9093,
+    "Version":"1.0.0.0"
+  }
+};
